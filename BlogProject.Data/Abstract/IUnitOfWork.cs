a17cbfecffbaf3780bfe2e4107a736589ff86038
denizduman1 <ce.denizduman2@gmail.com>
2022-03-10 13:38:15 +0300

    Unit Of Work Tasarım Deseni Projemize Uygulayalım

---
+++ BlogProject.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository ArticleRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        IUserRepository UserRepository { get; }
+        IRoleRepository RoleRepository { get; }
+        ICommentRepository CommentRepository { get; }
+        Task<int> SaveAsync();
+    }
+}
---
+++ BlogProject.Data/Concrete/EntityFramework/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using BlogProject.Data.Abstract;
+using BlogProject.Data.Concrete.EntityFramework.Contexts;
+using BlogProject.Data.Concrete.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogProject.Data.Concrete.EntityFramework
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogProjectContext _context;
+       
+        public UnitOfWork(BlogProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IArticleRepository ArticleRepository => new EfArticleRepository(_context);
+
+        public ICategoryRepository CategoryRepository => new EfCategoryRepository(_context);
+
+        public IUserRepository UserRepository => new EfUserRepository(_context);
+
+        public IRoleRepository RoleRepository => new EfRoleRepository(_context);
+
+        public ICommentRepository CommentRepository => new EfCommentRepository(_context);
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
